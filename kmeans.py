74b83f1edb71e2036c70a2c29dd100d576653df9
FunnyEngineer <57161781+FunnyEngineer@users.noreply.github.com>
2020-09-29 18:56:11 +0800

    Add files via upload

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,4 @@
-import sklearn
+from sklearn.cluster import KMeans
 from PIL import Image
 import numpy as np
 
@@ -9,3 +9,7 @@ print(image.size)
 print(image.mode)
 
 arr = np.asarray(image)
+arr = arr.reshape(-1, 3)
+new_arr = KMeans(n_clusters=2, random_state=0).fit_transform(arr)
+new_arr = new_arr.reshape(1024, 1024, 3)
+print(new_arr.shape)
